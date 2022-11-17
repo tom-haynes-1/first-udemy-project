176d8f64bb24dece926e42c366edd489e319ce49
Tom Haynes <tomhaynes94@gmail.com>
2022-11-17 08:15:28 +0000

    added event handlers for form submission component.

--- src/components/expenses/ExpenseForm.js
+++ src/components/expenses/ExpenseForm.js
@@ -5,7 +5,7 @@ const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-    const [addExpense, setAddExpense] = useState();
+    const [addExpense, setAddExpense] = useState('');
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
@@ -24,23 +24,49 @@ const ExpenseForm = () => {
 
     const onClickHandler = () => {
         setAddExpense(addExpense);
-        console.log('new Expense Added!');
+        console.log('New expense added!');
+    };
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
         
     return (
-        <form>
+        <form onSubmit={onSubmitHandler}>
           <div className="new-expense__controls">
             <div className="new-expense__control">
               <label>Title</label>
-              <input type="text" onChange={titleChangeHandler}/>
+              <input type="text" 
+                     value={enteredTitle}  
+                     onChange={titleChangeHandler}
+                />
             </div>
           <div className="new-expense__control">
               <label>Amount</label>
-              <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+              <input type="number"
+                     min="0.01" step="0.01"
+                     value={enteredAmount}
+                     onChange={amountChangeHandler} 
+                />
             </div>
             <div className="new-expense__control">
               <label>Date</label>
-              <input type="date" min="1900-01-01" step="2022-12-31" onChange={dateChangeHandler}/>
+              <input type="date" 
+                     min="1900-01-01" step="2022-12-31"
+                     value={enteredDate}
+                     onChange={dateChangeHandler}
+                />
             </div>
           </div>
           <div className="new-expense__actions">
