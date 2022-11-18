4b6a2bfbc729d2e7d2f4815672ffc5eb229ce5e8
Tom Haynes <tomhaynes94@gmail.com>
2022-11-18 17:24:21 +0000

    example commit

--- src/components/expenses/ExpenseForm.js
+++ src/components/expenses/ExpenseForm.js
@@ -46,7 +46,7 @@ const ExpenseForm = () => {
         <form onSubmit={onSubmitHandler}>
           <div className="new-expense__controls">
             <div className="new-expense__control">
-              <label>Title</label>
+              <label>Big Title</label>
               <input type="text" 
                      value={enteredTitle}  
                      onChange={titleChangeHandler}
