0c17cb0aa7e6e43f9eeef1029659595ca88971fc
Tom Haynes <tomhaynes94@gmail.com>
2023-02-11 19:30:33 +0000

    added 'key' prop mapped to the unique identifier of the array to <ExpenseItem /> so React can update the array more efficiently

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -24,13 +24,13 @@ const Expenses = (props) => {
       />
       {
           props.expenseItems.map((item) => ( 
-            <ExpenseItem 
+            <ExpenseItem
+              key={item.id}
               title={item.title} 
               amount={item.amount} 
               date={item.date} 
             />
-        ))
-      }
+      ))}
     </Card>
   </div>
   );
