9f74bda30213d8cdb894294c3582b81d2da185bc
Tom Haynes <tomhaynes94@gmail.com>
2023-02-11 18:28:20 +0000

    Conditionally rendered the 'expenses' array of objects from App.js using .map() method rather than hardcoding each individual <ExpenseItem /> component

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -22,26 +22,15 @@ const Expenses = (props) => {
         selected={ filteredYear }
         onShowYearHandler={ filterChangeHandler }
       />
-      <ExpenseItem
-        title={ props.expenses[0].title }
-        amount={ props.expenses[0].amount }
-        date={ props.expenses[0].date }
-      />
-      <ExpenseItem
-        title={ props.expenses[1].title }
-        amount={ props.expenses[1].amount }
-        date={ props.expenses[1].date }
-      />
-      <ExpenseItem
-        title={ props.expenses[2].title }
-        amount={ props.expenses[2].amount } 
-        date={ props.expenses[2].date }
-      />
-      <ExpenseItem
-        title={ props.expenses[3].title }
-        amount={ props.expenses[3].amount }
-        date={ props.expenses[3].date }
-      />
+      {
+          props.expenseItems.map((item) => ( 
+            <ExpenseItem 
+              title={item.title} 
+              amount={item.amount} 
+              date={item.date} 
+            />
+        ))
+      }
     </Card>
   </div>
   );
